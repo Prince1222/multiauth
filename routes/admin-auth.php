5cc57979f0494c20a3b603b280a50c0b7c95d4d6
Anik Maharjan <maharjanprincetag@gmail.com>
2025-01-31 11:33:58 +0545

    update

--- routes/admin-auth.php
+++ routes/admin-auth.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->prefix('admin')->name('admin.')->group(function () {
+Route::middleware('guest:admin')->prefix('admin')->name('admin.')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
@@ -19,10 +19,10 @@ Route::middleware('guest')->prefix('admin')->name('admin.')->group(function () {
 
 });
 
-Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
+Route::middleware('auth:admin')->prefix('admin')->name('admin.')->group(function () {
    
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('admin.dashboard');
     })->middleware(['verified'])->name('dashboard');
     
    
